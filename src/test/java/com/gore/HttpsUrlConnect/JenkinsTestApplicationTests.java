9c1d7f2aa274db6048b272581db3d7cb8aecfbb0
Vaibhav Gore <vaibhavgore2410@gmail.com>
2024-06-10 19:02:12 +0530

    Add testcase log for pipeline test

--- src/test/java/com/gore/HttpsUrlConnect/JenkinsTestApplicationTests.java
+++ src/test/java/com/gore/HttpsUrlConnect/JenkinsTestApplicationTests.java
@@ -1,16 +1,22 @@
 package com.gore.HttpsUrlConnect;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
 class JenkinsTestApplicationTests {
+	
+	public static Logger logger=LoggerFactory.getLogger(JenkinsTestApplication.class);
+
 
 	@Test
 	void contextLoads() {
 		assertEquals(true, true);
+		logger.info("Test Case executed!");
 	}
 
 }
